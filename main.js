9dc084b54cc5046e91423fb09c5b4d0058f20d98
tonamichan <sunday.gamer.tonami.ch@gmail.com>
2023-12-26 00:07:11 +0900

    Revert "test"
    This reverts commit 59e588adabdc8134c5c0455f3837da24fe529c8c.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // point.2 vue-router のルーティング
 const router = new VueRouter({
-  mode: 'history', // 入れたいけどgithub pagesで公開した時壊れる。なんで～
+  // mode: 'history', // 入れたいけどgithub pagesで公開した時壊れる。なんで～
   routes: [
     {
       path: "/",
